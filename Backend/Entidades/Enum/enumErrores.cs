fcd789052aab88af6d241fa4079cae91545e665e
Fabri0607UNA <fabricio.alfaro.arce@est.una.ac.cr>
2025-04-24 14:58:34 -0600

    Inclusion de Res y Req base, asi como los enum y la entidad error

---
+++ Backend/Entidades/Enum/enumErrores.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Entidades
+{
+    public enum enumErrores
+    {
+        excepcionBaseDatos = -2,
+        excepcionLogica = -1,
+        requestNulo = 1,
+        nombreFaltante = 2,
+        apellidoFaltante = 3,
+        correoFaltante = 4,
+        passwordFaltante = 5,
+        correoIncorrecto = 6,
+        passwordMuyDebil = 7,
+        idFaltante = 8,
+        sesionCerrada = 9,
+        verificacionFallida = 10,
+        verificacionExpirada = 11,
+        usuarioFaltante = 12,
+        temaFaltante = 13,
+        tituloFaltante = 14,
+        mensajeFaltante = 15,
+        idPublicacionFaltante = 16
+
+    }
+}
---
+++ Backend/Entidades/Request/ReqBase.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Entidades.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Entidades
+{
+    public class ReqBase
+    {
+        public Sesion sesion { get; set; }
+    }
+}
