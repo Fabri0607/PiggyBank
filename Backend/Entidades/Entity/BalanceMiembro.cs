49e82656a4654ee95631bf25ce3567aa40e0704a
Fabri0607UNA <160906299+Fabri0607UNA@users.noreply.github.com>
2025-04-28 10:51:48 -0600

    Inclusión de La entidad BalanceMiembro
    Inclusión de La entidad BalanceMiembro

---
+++ Backend/Entidades/Entity/BalanceMiembro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Entidades.Entity
+{
+    public class BalanceMiembro
+    {
+        public int BalanceID { get; set; }
+        public int GrupoID { get; set; }
+        public int UsuarioID { get; set; }
+        public string NombreUsuario { get; set; } // Para incluir en respuestas, se llena desde la consulta
+        public decimal TotalGastos { get; set; }
+        public decimal TotalPagado { get; set; }
+        public decimal Saldo { get; set; }
+        public DateTime FechaCalculo { get; set; }
+    }
+}
